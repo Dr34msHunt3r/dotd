36c803cf9db6c1f21ef9e7e8004f9025de8a963c
Dr34msHunt3r <an.miller@wp.pl>
2021-11-22 10:58:37 +0100

    Removed warnings

--- lib/presentation/screens/edit_recipe_screen.dart
+++ lib/presentation/screens/edit_recipe_screen.dart
@@ -4,10 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-import '../router.dart';
-
 class EditRecipeScreen extends StatefulWidget {
-  EditRecipeScreen({Key? key, required this.recipe}) : super(key: key);
+  const EditRecipeScreen({Key? key, required this.recipe}) : super(key: key);
 
   final Recipe recipe;
 
