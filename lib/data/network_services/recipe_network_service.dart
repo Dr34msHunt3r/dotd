bc34f8a5c95beb4a9529f8707024a0b9c6d2986d
Dr34msHunt3r <an.miller@wp.pl>
2021-12-18 21:48:00 +0100

    init: init

--- lib/data/network_services/recipe_network_service.dart
+++ lib/data/network_services/recipe_network_service.dart
@@ -1,17 +1,20 @@
 import 'dart:convert';
 
-import 'package:http/http.dart' as http;
+import 'package:dotd/data/network_services/http_service.dart';
+import 'package:http/http.dart' as https;
 
 class RecipeNetworkService {
 
+  final HttpService http = HttpService();
   final baseUrl = "http://10.0.2.2:3000";
-  // final baseUrl = "http://192.168.1.8:3000";
 
   Future<List<dynamic>> fetchRecipes() async {
     try{
-      final response = await http.get(Uri.parse(baseUrl + "/recipes/get"));
-      print(response.body);
-      return jsonDecode(response.body) as List;
+      final response = await http.getRequest("/recipes/get");
+      if(response.statusCode ==200){}else{
+        print(response.statusCode);
+      }
+      return jsonDecode(response.data) as List;
     }catch(e){
       print(e);
       return [];
@@ -20,7 +23,7 @@ class RecipeNetworkService {
 
   Future<Map<String, dynamic>> addRecipe(String recipeObj) async {
     try{
-      final response = await http.post(Uri.parse(baseUrl + "/recipes/add"), body: {"recipe": recipeObj});
+      final response = await https.post(Uri.parse(baseUrl + "/recipes/add"), body: {"recipe":recipeObj});
       return jsonDecode(response.body);
     }catch(e){
       print(e);
@@ -30,7 +33,7 @@ class RecipeNetworkService {
 
   Future<bool> deleteRecipe(String id) async{
     try{
-      await http.delete(Uri.parse(baseUrl + "/recipes/delete:id"), body: {"id": id});
+      await https.delete(Uri.parse(baseUrl + "/recipes/delete:id"), body: {"id": id});
       return true;
     }catch(e){
       print(e);
@@ -40,7 +43,7 @@ class RecipeNetworkService {
 
   Future<bool> putRecipe(String putObj) async{
     try{
-      await http.put(Uri.parse(baseUrl + "/recipes/put:id"), body: {"recipe": putObj});
+      await https.put(Uri.parse(baseUrl + "/recipes/put:id"), body: {"recipe": putObj});
       return true;
     }catch(e){
       print(e);
