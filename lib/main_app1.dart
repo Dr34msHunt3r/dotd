d4db5d50d73aa44273083063a9332d2130def31f
Dr34msHunt3r <an.miller@wp.pl>
2022-01-31 13:56:09 +0100

    feat: mains with flavor configured for new navigation auto route

--- lib/main_app1.dart
+++ lib/main_app1.dart
@@ -1,16 +1,20 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
+import 'base/app_config.dart';
 import 'base/firebase_remote_config.dart';
 import 'extensions/flavor_config.dart';
-import 'base/home.dart';
-import 'navigation/router.dart';
 
-void main() async{
+import 'navigation/auto_router.gr.dart';
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   FirebaseRemoteConfig config = FirebaseRemoteConfig();
   await config.initialise();
+  await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
   FlavorConfig(flavor: Flavor.RESTAPI,
       values: FlavorValues(source: "Rest API"));
-  runApp(DoTD(router: AppRouter(), config: config));
+  runApp(DoTDConfig(config: config, appRoute: AppRouter(),));
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,9 +5,9 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:dotd/base/app_config.dart';
 import 'package:dotd/base/firebase_remote_config.dart';
-import 'package:dotd/base/home.dart';
-import 'package:dotd/navigation/router.dart';
+import 'package:dotd/navigation/auto_router.gr.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -15,7 +15,7 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget( DoTD(router: AppRouter(), config: FirebaseRemoteConfig()));
+    await tester.pumpWidget( DoTDConfig(appRoute: AppRouter(), config: FirebaseRemoteConfig()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
